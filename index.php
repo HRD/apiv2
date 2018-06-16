c26ae4c245e6a29c46e44af50a143290c2e60e15
Sławomir Kaleta <slaszka@gmail.com>
2018-06-16 20:36:12 +0200

    add example

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+use HRDBase\Api\HRDApi;
+
+include_once 'vendor/autoload.php';
+
+$apiInstance = HRDApi::getInstance([
+    'apiHash' => '__hash__',
+    'apiLogin' => '__login__',
+    'apiPass' => '__haslo_api__',
+]);
